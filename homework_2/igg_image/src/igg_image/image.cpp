b2be170d91d0b5fb7ed89921bf92bc898e3e0730
Dusan Bajovic <dusan.bajovic@yahoo.com>
2023-08-12 17:51:37 +0200

    First version of second homework

---
+++ homework_2/igg_image/src/igg_image/image.cpp
@@ -0,0 +1,102 @@
+#include <image.h>
+#include <io_tools.h>
+
+bool Image::FillFromPgm(const std::string &file_name) {
+  igg::io_tools::ImageData readImageData;
+
+  readImageData = ReadFromPgm(file_name);
+
+  if (readImageData.data.empty()) {
+    return false;
+  }
+
+  rows_ = readImageData.rows;
+  cols_ = readImageData.cols;
+  max_val_ = readImageData.max_val;
+
+  copy(readImageData.data.begin(), readImageData.data.end(),
+       back_insterter(data_));
+
+  return true;
+}
+
+void Image::WriteToPgm(const std::string &file_name) const {
+  igg::io_tools::ImageData writeImageData;
+
+  writeImageData.rows = rows_;
+  writeImageData.cols = cols_;
+  writeImageData.max_val = max_val_;
+  writeImageData.data = data_
+
+      if (WriteToPgm(&writeImageData, file_name) == false) {
+  std:
+    cout << "Image data is not written!!!" << std::endl;
+  }
+}
+
+std::vector<float> Image::ComputeHistogram(int bins) const {
+  std::vector<float> histogram(bins);
+  int bin_number = 0;
+
+  if (bins < max_val_) {
+    for (bin_number = 0; bin_number < bins; ++bin_number) {
+      histogram[bin_number] = 0;
+      for (const int &eachPixel : data_) {
+        if ((eachPixel >= (bin_number * max_val_ / bins)) &&
+            (eachPixel < ((bin_number + 1) * max_val_ / bins))) {
+          historgram[bin_number] += 1;
+        }
+      }
+      historgram[bin_number] /= (rows_ * cols_);
+    }
+  }
+
+  return histogram;
+}
+
+void igg::Image::DownScale(int scale)
+{
+    int rows_new = rows_/scale;
+    int cols_new = cols_/scale;
+    std::vector<int> data_new(rows_new * cols_new);
+    int p_rows, p_cols;
+
+    for(int i = 0; i<rows_new; ++i)
+    {
+        for(int j = 0; j<cols_new; ++j)
+        {
+            p_rows = i*scale;
+            p_cols = j*scale;
+            data_new[(j*cols_new) + i] = data_[(p_cols*cols_) + p_rows];  
+        } 
+    }
+
+    rows_ = rows_new;
+    cols_ = cols_new;
+    data_.resize(rows_*cols_);
+    data_ = data_new;
+}
+
+void Image::UpScale(int scale)
+{
+    int rows_new = rows_*scale;
+    int cols_new = cols_*scale;
+    std::vector<int> data_new(rows_new * cols_new);
+    int p_rows, p_cols;
+
+    for(int i = 0; i<rows_new; ++i)
+    {
+        for(int j = 0; j<cols_new; ++j)
+        {
+            p_rows = i/scale;
+            p_cols = j/scale;
+            data_new[(j*cols_new) + i] = data_[(p_cols*cols_) + p_rows];  
+        } 
+    }
+
+    rows_ = rows_new;
+    cols_ = cols_new;
+    data_.resize(rows_*cols_);
+    data_ = data_new;
+
+}
--- homework_2/igg_image/src/igg_image/image.h
+++ homework_2/igg_image/src/igg_image/image.h
@@ -7,6 +7,15 @@ namespace igg {
 class Image {
  public:
   ///////////////////// Create the public interface here ///////////////////////
+  Image();
+  Image(int rows, int cols) : rows_{rows}, cols_{cols} {
+    data_.reserve(rows*cols);
+  }
+  int rows() const {return rows_;}
+  int cols() const {return cols_;}
+  int& at(const int row, const int col) {
+    return data_.at(row*cols_ + col -1);
+  }
  private:
   int rows_ = 0;
   int cols_ = 0;
